a3539d6b2f8e74b7ea8d1383e86fe4f0f6d70670
josemoreca01-alt <josemoreca01@gmail.com>
2025-10-29 13:44:44 -0600

    Delete zen-web directory

--- zen-web/assets/adblock.js
+++
@@ -1,9 +0,0 @@
-// Bloqueo básico de anuncios por clase e ID
-document.addEventListener("DOMContentLoaded", () => {
-  const selectors = [
-    ".ad", ".ads", "#ad", "#ads", ".banner", ".sponsored", "[id^='ad-']"
-  ];
-  selectors.forEach(sel => {
-    document.querySelectorAll(sel).forEach(el => el.remove());
-  });
-});
--- zen-web/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Compila Zen usando pkg-config para detectar WebKitGTK y GTK
-gcc src/zen.c -o zen $(pkg-config --cflags --libs webkit2gtk-4.0 gtk+-3.0)
-gcc src/zen_kiosk.c -o zen_kiosk $(pkg-config --cflags --libs webkit2gtk-4.0 gtk+-3.0)
-gcc src/zen-web.c -o zen-web `pkg-config --cflags --libs gtk+-3.0 webkit2gtk-4.0`
